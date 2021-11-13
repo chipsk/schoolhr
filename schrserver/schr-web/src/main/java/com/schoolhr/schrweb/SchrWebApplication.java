d746da83a0a30fd5f419e2a696245e82479d567e
zzl514 <1146942367@qq.com>
2021-11-13 23:25:18 +0800

    Initial commit

---
+++ schrserver/schr-web/src/main/java/com/schoolhr/schrweb/SchrWebApplication.java
@@ -0,0 +1,13 @@
+package com.schoolhr.schrweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchrWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchrWebApplication.class, args);
+    }
+
+}
---
+++ schrserver/schr-web/src/test/java/com/schoolhr/schrweb/SchrWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolhr.schrweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchrWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
