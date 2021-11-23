2a07f81565e4ab256aef30f846ee194571ad7177
liao <2473536852@qq.com>
2021-11-23 10:46:46 +0800

    add GetIDSalary

---
+++ schrserver/schr-mapper/src/main/java/com/schoolhr/mapper/SalaryMapper.java
@@ -0,0 +1,24 @@
+package com.schoolhr.mapper;
+
+import com.schoolhr.model.Salary;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SalaryMapper {
+
+    int deleteByPrimaryKey(Integer uid);
+
+    int insert(Salary record);
+
+    int insertSelective(Salary record);
+
+    Salary selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Salary record);
+
+    int updateByPrimaryKey(Salary record);
+
+    List<Salary> getAllSalaries();
+    List<Salary> getIDSalaries(@Param("userID") Integer userID);
+}
--- schrserver/schr-model/src/main/java/com/schoolhr/model/Salary.java
+++ schrserver/schr-model/src/main/java/com/schoolhr/model/Salary.java
@@ -1,4 +1,86 @@
 package com.schoolhr.model;
+import java.util.Date;
 
 public class Salary {
+    private Integer uid;
+    private Integer userID;
+    private String username;
+    private Integer departmentid;
+    private Integer basisSalary;
+    private String allSalary;
+    private String bonus;
+    private Date date;
+    private String remark;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getDepartmentid() {
+        return departmentid;
+    }
+
+    public Integer getBasisSalary() {
+        return basisSalary;
+    }
+
+    public String getAllSalary() {
+        return allSalary;
+    }
+
+    public String getBonus() {
+        return bonus;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setDepartmentid(Integer departmentid) {
+        this.departmentid = departmentid;
+    }
+
+    public void setBasisSalary(Integer basisSalary) {
+        this.basisSalary = basisSalary;
+    }
+
+    public void setAllSalary(String allSalary) {
+        this.allSalary = allSalary;
+    }
+
+    public void setBonus(String bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
 }
